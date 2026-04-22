4e22b872fd3030d53eb3d8085ce98e9c8382c50b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:58 -0500

    TUnit conversion

--- test/Soenneker.Extensions.HttpRequestDatas.Tests/HttpRequestDataExtensionTests.cs
+++ test/Soenneker.Extensions.HttpRequestDatas.Tests/HttpRequestDataExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.HttpRequestDatas.Tests;
 
 public sealed class HttpRequestDataExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
